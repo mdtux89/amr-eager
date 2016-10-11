f9b21cc5953a9ea19b791a0c989a0631a8c3164b
mdtux89 <mdtux89@gmail.com>
2016-10-11 14:35:40 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 wget http://kinloch.inf.ed.ac.uk/public/direct/ameager/resources.tar.gz
 tar -xaf resources.tar.gz
 
+wget http://kinloch.inf.ed.ac.uk/public/direct/ameager/LDC2015E86.tar.gz
+tar -xaf LDC2015E86.tar.gz
+
 wget https://github.com/redpony/cdec/archive/master.zip
 unzip master.zip
 
