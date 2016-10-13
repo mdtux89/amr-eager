a7e6e9df34bdb2f1521d217e4c9ba9998b623fe7
mdtux89 <mdtux89@gmail.com>
2016-10-13 16:25:58 +0100

    updated evaluation.sh and st else

--- amrevaluation/evaluation.sh
+++ amrevaluation/evaluation.sh
@@ -18,8 +18,8 @@ out=`python smatch_2.0.2/smatch.py --pr -f 1.tmp 2.tmp`
 out=($out)
 echo 'No WSD -> P: '${out[1]}', R: '${out[3]}', F: '${out[6]} | sed 's/.$//'
 
-cat "$1" | perl -ne 's/-(\d+(\.\d+)?)/\1/g; print;' | perl -ne 's/,/\./g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 1.tmp
-cat "$2" | perl -ne 's/-(\d+(\.\d+)?)/\1/g; print;' | perl -ne 's/,/\./g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 2.tmp
+cat "$1" | perl -ne 's/[^a-z\"]-(\d+(\.\d+)?)/\1/g; print;' | perl -ne 's/,/\./g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 1.tmp
+cat "$2" | perl -ne 's/[^a-z\"]-(\d+(\.\d+)?)/\1/g; print;' | perl -ne 's/,/\./g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 2.tmp
 
 python scores.py "1.tmp" "2.tmp"
 
